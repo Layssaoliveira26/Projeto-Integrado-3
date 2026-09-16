5263bb8bd418af38f5ed0bab8d740818a7a6e00b
Mel Barbosa Tamboril <meltamboril@gmail.com>
2026-09-16 11:52:14 -0300

    feat(navigation): implementa fluxo de navegacao inicial da aplicacao

---
+++ mobile/src/screens/Home.js
@@ -0,0 +1,15 @@
+import { View, Text, Button, StyleSheet } from "react-native";
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home</Text>
+      <Button title="Sair" onPress={() => navigation.replace("Login")} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  title: { fontSize: 24 },
+});
